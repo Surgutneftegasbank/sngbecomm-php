96cbea6fb1e9ec4c17e6aebe859b3e704bcb0290
Yura Zatsepin <yu.zatsepin@sngb.ru>
2014-01-24 11:06:06 +0600

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+0.1
+-----
+* Initial git for VC ([@zatsepin](mailto:yu.zatsepin@sngb.ru))
---
+++ lib/SNGBEcomm.php
@@ -0,0 +1,23 @@
+<?php
+
+// Tested on PHP 5.3
+
+if (!function_exists('curl_init')) {
+    throw new Exception('SNGBEcomm needs the CURL PHP extension.');
+}
+//if (!function_exists('json_decode')) {
+    //throw new Exception('Stripe needs the JSON PHP extension.');
+//}
+//if (!function_exists('mb_detect_encoding')) {
+    //throw new Exception('Stripe needs the Multibyte String PHP extension.');
+//}
+
+// Stripe singleton
+//require(dirname(__FILE__) . '/SNGBEcomm/SNGBEcomm.php');
+
+// Utilities
+//require(dirname(__FILE__) . '/SNGBEcomm/Util.php');
+//require(dirname(__FILE__) . '/SNGBEcomm/Util/Set.php');
+
+// Errors
+//require(dirname(__FILE__) . '/SNGBEcomm/Error.php');
